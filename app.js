de540ffbc17faf319ea9a6b7ae4db99a733ffa53
muhammadhussainharoon <hussain356q@gmail.com>
2023-10-02 02:26:19 +0500

    done

--- app.js
+++ app.js
@@ -7,3 +7,6 @@ div.addEventListener('mouseover', function(){
       div.innerHTML = '<img src="./asset/bulb on.webp" alt="bulb on">';
 
 })
+div.addEventListener('mouseout' , function(){
+      div.innerHTML = '<img src="./asset/bulb off.jpg" alt="">'
+})
